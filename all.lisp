089bae3ed51253adca41d4e9963c25c5c958a8fe
Paul Tarvydas <guitarvydas@gmail.com>
2020-04-23 16:18:28 -0400

    all.lisp added

---
+++ all.lisp
@@ -0,0 +1,47 @@
+(ql:quickload :parsing-assembler)
+
+;; stack-dsl
+
+(uiop:run-program "rm -rf ~/.cache/common-lisp ;
+ rm -rf ~/quicklisp/local-projects/*.fasl ;
+ rm -rf ~/quicklisp/local/projects/*~")
+
+(ql:quickload :stack-dsl/generate)
+
+(stack-dsl::generate)
+
+
+;; sm-dsl code generation
+ (uiop:run-program "rm -rf ~/.cache/common-lisp ;
+ rm -rf ~/quicklisp/local-projects/*/*.fasl ;
+ rm -rf ~/quicklisp/local/projects/*/*~")
+(ql:quickload :sm-dsl)
+(load (sm-dsl::path "generate-stacks.lisp"))
+(sm-dsl::generate-stacks)))
+(uiop:run-program "rm -rf ~/.cache/common-lisp ;
+  rm -rf ~/quicklisp/local-projects/*/*.fasl ;
+  rm -rf ~/quicklisp/local/projects/*/*~")
+(ql:quickload :sm-dsl/generate)
+(sm-dsl::generate "SM-DSL")
+
+;; sm-dsl
+ (uiop:run-program "rm -rf ~/.cache/common-lisp ;
+ rm -rf ~/quicklisp/local-projects/*.fasl ;
+ rm -rf ~/quicklisp/local/projects/*~")
+ (ql:quickload :sm-dsl)
+   (load (sm-dsl::path "stacks.lisp"))  #| stacks.lisp is generated above |#
+   (load (sm-dsl::path "classes.lisp"))
+   (load (sm-dsl::path "macros.lisp"))
+   (load (sm-dsl::path "sm-dsl.lisp"))  #| sm-dsl.lisp is generated above |#
+  (load (sm-dsl::path "mechanisms.lisp"))
+  (load (sm-dsl::path "transpile.lisp"))
+
+
+;; producer-consumer
+ (uiop:run-program "rm -rf ~/.cache/common-lisp ;
+ rm -rf ~/quicklisp/local-projects/*.fasl ;
+ rm -rf ~/quicklisp/local/projects/*~")
+ (ql:quickload :producer-consumer)
+  (load (producer-consumer::path "generate.lisp"))
+  (producer-consumer::generate)
+
